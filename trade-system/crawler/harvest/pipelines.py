20fdd8fac2d0944933a1b3693b4f6927f775279d
Deep Dalsaniya <155941115+Deepdalsaniya@users.noreply.github.com>
2025-09-24 15:33:14 -0400

    Update pipelines.py

--- trade-system/crawler/harvest/pipelines.py
+++ trade-system/crawler/harvest/pipelines.py
@@ -1,1 +1,14 @@
+# Basic Scrapy settings:
+# - respect robots.txt
+# - slow down a bit so we don't hammer sites
+# - send items to Postgres via our pipeline
+
+BOT_NAME = "harvest"
+
+ROBOTSTXT_OBEY = True
+DOWNLOAD_DELAY = 1.0
+
+ITEM_PIPELINES = {
+    "harvest.pipelines.PostgresPipeline": 300
+}
 
