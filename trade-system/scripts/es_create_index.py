362199feca2735ee2c16ff80bb8cb3553de09828
Deep Dalsaniya <155941115+Deepdalsaniya@users.noreply.github.com>
2025-09-24 15:32:16 -0400

    Update es_create_index.py

--- trade-system/scripts/es_create_index.py
+++ trade-system/scripts/es_create_index.py
@@ -1,1 +1,30 @@
+# Create an Elasticsearch index to store our docs for keyword search.
+
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(hosts=["http://localhost:9200"])
+idx = "compliance_docs"
+
+mapping = {
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "default": { "type":"standard" }
+      }
+    }
+  },
+  "mappings": {
+    "properties": {
+      "url":   {"type":"keyword"},
+      "title": {"type":"text"},
+      "text":  {"type":"text"}
+    }
+  }
+}
+
+if es.indices.exists(index=idx):
+    print("Index already exists")
+else:
+    es.indices.create(index=idx, body=mapping)
+    print("Created index:", idx)
 
